aa1a3f464b1901aa44f24c7868ac617446e12a7d
TMurMur <8060750@gmail.com>
2022-05-05 10:20:15 +0300

    masterpieces

---
+++ matplotlib_styles.py
@@ -0,0 +1,4 @@
+import matplotlib.pyplot as plt
+plt.style.available
+
+['Solarize_Light2', '_classic_test_patch', '_mpl-gallery', '_mpl-gallery-nogrid', 'bmh', 'classic', 'dark_background', 'fast', 'fivethirtyeight', 'ggplot', 'grayscale', 'seaborn', 'seaborn-bright', 'seaborn-colorblind', 'seaborn-dark', 'seaborn-dark-palette', 'seaborn-darkgrid', 'seaborn-deep', 'seaborn-muted', 'seaborn-notebook', 'seaborn-paper', 'seaborn-pastel', 'seaborn-poster', 'seaborn-talk', 'seaborn-ticks', 'seaborn-white', 'seaborn-whitegrid', 'tableau-colorblind10']
--- readme.txt
+++ readme.txt
@@ -5,5 +5,9 @@ installs Python packages. Enter the following command at a
 terminal prompt:
 $ python -m pip install --user matplotlib
 
+***********
+Чтобы добавить путь к вашей установке POPH к вашей переменной PATH , вы можете использовать панель управления или команду setx . Например:
+setx PATH "%PATH%;C:\Users\ TIM \AppData\Roaming\Python\Python39\Scripts" 
+
 
 https://matplotlib.org/stable/gallery/index.html
